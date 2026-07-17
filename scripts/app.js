c8584519ad282d4d3fe8e8201bed1f23b93b2fde
shahab <sano.manjiro7513@gmail.com>
2026-07-17 06:01:41 -0700

    item slider section created

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,30 @@
-const swiper = new Swiper('.swiper', {
-  
-  direction: 'vertical',
-  loop: true,
-  
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+var swiper = new Swiper('.mySwiper', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          390: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          540: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          992: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1200: {
+            slidesPerView: 6,
+            spaceBetween: 50,
+          },
+        },
+      });
